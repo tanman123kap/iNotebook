bc546b0614d9dc74821101a63e631c6e95bc178f
tanman123kap <tanman123kap@gmail.com>
2025-04-28 00:16:47 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectMongoose = () => {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
+const connectMongoose = async () => {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("Database Connected...");
     }).catch((error) => {
         console.log(error.message);
